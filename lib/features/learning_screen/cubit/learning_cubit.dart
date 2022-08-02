a37339bc74a013d82dd36a88788b39562fbda198
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-08-02 22:37:31 +0300

    delete unnecessary comments

--- lib/features/learning_screen/cubit/learning_cubit.dart
+++ lib/features/learning_screen/cubit/learning_cubit.dart
@@ -23,7 +23,6 @@ class LearningCubit extends Cubit<LearningState> {
   void nextWord() {
     var curIndex = state.currentIndex;
     if (state.isCurrentWordLast) {
-      //curIndex = 0;
       return;
     } else {
       curIndex = curIndex + 1;
@@ -34,80 +33,10 @@ class LearningCubit extends Cubit<LearningState> {
   void prevWord() {
     var curIndex = state.currentIndex;
     if (state.isCurrentWordFirst) {
-      //curIndex = state.dailyWords.length;
       return;
     } else {
       curIndex = curIndex - 1;
     }
     emit(state.copyWith(indexCurrentWord: curIndex));
   }
-
-  // void getWords() {
-  //   emit(
-  //     const LearningState.loading(),
-  //   );
-
-  //   final dailyWords = _currentWordRepository.getDailyWords();
-
-  //   emit(
-  //     LearningState.loaded(
-  //       dailyWords: dailyWords,
-  //     ),
-  //   );
-  // }
-
-  // int? get currentIndex {
-  //   final index = state.mapOrNull(loaded: (state) => state.indexCurrentWord);
-  //   return index;
-  // }
-
-  // OneWord? get currentWord {
-  //   final word = state.mapOrNull(
-  //       loaded: (state) => state.dailyWords[state.indexCurrentWord],);
-  //   return word;
-  // }
-
-  // bool get isCurrentWordFirst {
-  //   return currentIndex == 0;
-  // }
-
-  //   bool get isCurrentWordLast {
-  //   final length = state.mapOrNull(loaded: (state) => state.dailyWords.length);
-  //   return currentIndex == (length!-1);
-  // }
-
-  // void nextWord() {
-  //   var index = currentIndex;
-  //   if (isCurrentWordLast) {
-  //     index = 0;
-  //   } else {
-  //     index = index! + 1;
-  //   }
-
-  //   final words = state.mapOrNull(loaded: (state) => state.dailyWords);
-
-  //   emit(
-  //     LearningState.loaded(
-  //       indexCurrentWord: index,
-  //       dailyWords: words!,
-  //     ),
-  //   );
-  // }
-
-  // void prevWord() {
-  //   var index = currentIndex;
-  //   if (isCurrentWordFirst) {
-  //     final length = state.mapOrNull(loaded: (state) => state.dailyWords.length);
-  //     index = length;
-  //   } else {
-  //     index = index! - 1;
-  //   }
-  //   final words = state.mapOrNull(loaded: (state) => state.dailyWords);
-  //   emit(
-  //     LearningState.loaded(
-  //       indexCurrentWord: index!,
-  //       dailyWords: words!,
-  //     ),
-  //   );
-  // }
 }
