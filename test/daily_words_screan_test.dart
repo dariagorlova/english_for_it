210a89d8176c426e56a51720a0fcbd20ca9b33fa
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-08-02 22:30:35 +0300

    reworking of tests using mock classes

--- test/daily_words_screan_test.dart
+++ test/daily_words_screan_test.dart
@@ -6,12 +6,14 @@ import 'package:flutter_test/flutter_test.dart';
 
 import './step/the_app_is_running.dart';
 import './step/i_tap_text.dart';
+import './step/i_wait_second.dart';
 import './step/i_see_text.dart';
 
 void main() {
   Future<void> bddSetUp(WidgetTester tester) async {
     await theAppIsRunning(tester);
     await iTapText(tester, "Let's start");
+    await iWaitSecond(tester, 1);
   }
   group('''Learning words''', () {
     testWidgets('''''', (tester) async {
--- test/start_screen.feature
+++ test/start_screen.feature
@@ -9,6 +9,7 @@ Feature: Start of application
 
   Scenario: As a User I tap a button
     And I tap {"Let's start"} text
+    # And I wait {1} second
     And I wait
     Then I don't see {"Let's start"} text
 
