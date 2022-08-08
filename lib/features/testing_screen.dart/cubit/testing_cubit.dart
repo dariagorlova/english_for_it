33ca686ae256cbdf0c8ebef5cbc715ad3678c43d
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-08-08 17:56:37 +0300

    add cubit for testing screen

---
+++ lib/features/testing_screen.dart/cubit/testing_cubit.dart
@@ -0,0 +1,93 @@
+import 'dart:math';
+import 'package:english_for_it/core/model/one_word.dart';
+import 'package:english_for_it/core/model/word_with_answers.dart';
+import 'package:english_for_it/features/testing_screen.dart/cubit/testing_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable
+class TestingCubit extends Cubit<TestingState> {
+  TestingCubit(
+    @factoryParam List<OneWord> words,
+  ) : super(
+          const TestingState(
+            wordsWithAnswers: [],
+            indexCurrentWord: 0,
+            answerTried: [],
+          ),
+        ) {
+    init(words);
+  }
+
+  void init(List<OneWord> dailyWords) {
+    if (dailyWords.isEmpty) return;
+
+    final listTranslations = dailyWords
+        .map(
+          (word) => word.translate,
+        )
+        .toList();
+
+    final words = dailyWords.map(
+      (w) {
+        final answers = <String>[w.translate];
+        while (true) {
+          final tmpWord = _getRandomElement(listTranslations);
+          if (!answers.contains(tmpWord)) {
+            answers.add(tmpWord);
+          }
+          if (answers.length == 4) {
+            break;
+          }
+        }
+        answers.shuffle();
+
+        return WordWithAnswers(
+          word: w.word,
+          answers: answers,
+          indexOfCorrectAnswer: answers.indexWhere(
+            (element) => element == w.translate,
+          ),
+        );
+      },
+    ).toList();
+    final listAnswers = List.generate(4, (_) => false);
+
+    emit(
+      TestingState(wordsWithAnswers: words, answerTried: listAnswers),
+    );
+  }
+
+  String _getRandomElement<String>(List<String> allWords) {
+    final i = Random().nextInt(allWords.length);
+    return allWords[i];
+  }
+
+  bool checkChoise(int choise) {
+    if (state.indexOfCorrectAnswerForCurrentWord != choise) {
+      var listAnswers = state.answerTried;
+      //listAnswers[choise] = true;
+
+      var list2 = <bool>[];
+      for (var i = 0; i < listAnswers.length; i++) {
+        if (i == choise) {
+          list2.add(true);
+        } else {
+          list2.add(listAnswers[i]);
+        }
+      }
+
+      emit(state.copyWith(answerTried: list2));
+      return false;
+    } else {
+      final listNew = List.generate(4, (index_) => false);
+
+      emit(
+        state.copyWith(
+            indexCurrentWord: state.currentIndex + 1, answerTried: listNew),
+      );
+
+      return true;
+    }
+  }
+}
---
+++ lib/features/testing_screen.dart/cubit/testing_state.freezed.dart
@@ -0,0 +1,194 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'testing_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$TestingState {
+  List<WordWithAnswers> get wordsWithAnswers =>
+      throw _privateConstructorUsedError;
+  int get indexCurrentWord => throw _privateConstructorUsedError;
+  List<bool> get answerTried => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $TestingStateCopyWith<TestingState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $TestingStateCopyWith<$Res> {
+  factory $TestingStateCopyWith(
+          TestingState value, $Res Function(TestingState) then) =
+      _$TestingStateCopyWithImpl<$Res>;
+  $Res call(
+      {List<WordWithAnswers> wordsWithAnswers,
+      int indexCurrentWord,
+      List<bool> answerTried});
+}
+
+/// @nodoc
+class _$TestingStateCopyWithImpl<$Res> implements $TestingStateCopyWith<$Res> {
+  _$TestingStateCopyWithImpl(this._value, this._then);
+
+  final TestingState _value;
+  // ignore: unused_field
+  final $Res Function(TestingState) _then;
+
+  @override
+  $Res call({
+    Object? wordsWithAnswers = freezed,
+    Object? indexCurrentWord = freezed,
+    Object? answerTried = freezed,
+  }) {
+    return _then(_value.copyWith(
+      wordsWithAnswers: wordsWithAnswers == freezed
+          ? _value.wordsWithAnswers
+          : wordsWithAnswers // ignore: cast_nullable_to_non_nullable
+              as List<WordWithAnswers>,
+      indexCurrentWord: indexCurrentWord == freezed
+          ? _value.indexCurrentWord
+          : indexCurrentWord // ignore: cast_nullable_to_non_nullable
+              as int,
+      answerTried: answerTried == freezed
+          ? _value.answerTried
+          : answerTried // ignore: cast_nullable_to_non_nullable
+              as List<bool>,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$$_TestingStateCopyWith<$Res>
+    implements $TestingStateCopyWith<$Res> {
+  factory _$$_TestingStateCopyWith(
+          _$_TestingState value, $Res Function(_$_TestingState) then) =
+      __$$_TestingStateCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {List<WordWithAnswers> wordsWithAnswers,
+      int indexCurrentWord,
+      List<bool> answerTried});
+}
+
+/// @nodoc
+class __$$_TestingStateCopyWithImpl<$Res>
+    extends _$TestingStateCopyWithImpl<$Res>
+    implements _$$_TestingStateCopyWith<$Res> {
+  __$$_TestingStateCopyWithImpl(
+      _$_TestingState _value, $Res Function(_$_TestingState) _then)
+      : super(_value, (v) => _then(v as _$_TestingState));
+
+  @override
+  _$_TestingState get _value => super._value as _$_TestingState;
+
+  @override
+  $Res call({
+    Object? wordsWithAnswers = freezed,
+    Object? indexCurrentWord = freezed,
+    Object? answerTried = freezed,
+  }) {
+    return _then(_$_TestingState(
+      wordsWithAnswers: wordsWithAnswers == freezed
+          ? _value._wordsWithAnswers
+          : wordsWithAnswers // ignore: cast_nullable_to_non_nullable
+              as List<WordWithAnswers>,
+      indexCurrentWord: indexCurrentWord == freezed
+          ? _value.indexCurrentWord
+          : indexCurrentWord // ignore: cast_nullable_to_non_nullable
+              as int,
+      answerTried: answerTried == freezed
+          ? _value._answerTried
+          : answerTried // ignore: cast_nullable_to_non_nullable
+              as List<bool>,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_TestingState implements _TestingState {
+  const _$_TestingState(
+      {final List<WordWithAnswers> wordsWithAnswers = const [],
+      this.indexCurrentWord = 0,
+      final List<bool> answerTried = const []})
+      : _wordsWithAnswers = wordsWithAnswers,
+        _answerTried = answerTried;
+
+  final List<WordWithAnswers> _wordsWithAnswers;
+  @override
+  @JsonKey()
+  List<WordWithAnswers> get wordsWithAnswers {
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_wordsWithAnswers);
+  }
+
+  @override
+  @JsonKey()
+  final int indexCurrentWord;
+  final List<bool> _answerTried;
+  @override
+  @JsonKey()
+  List<bool> get answerTried {
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_answerTried);
+  }
+
+  @override
+  String toString() {
+    return 'TestingState(wordsWithAnswers: $wordsWithAnswers, indexCurrentWord: $indexCurrentWord, answerTried: $answerTried)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_TestingState &&
+            const DeepCollectionEquality()
+                .equals(other._wordsWithAnswers, _wordsWithAnswers) &&
+            const DeepCollectionEquality()
+                .equals(other.indexCurrentWord, indexCurrentWord) &&
+            const DeepCollectionEquality()
+                .equals(other._answerTried, _answerTried));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(_wordsWithAnswers),
+      const DeepCollectionEquality().hash(indexCurrentWord),
+      const DeepCollectionEquality().hash(_answerTried));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_TestingStateCopyWith<_$_TestingState> get copyWith =>
+      __$$_TestingStateCopyWithImpl<_$_TestingState>(this, _$identity);
+}
+
+abstract class _TestingState implements TestingState {
+  const factory _TestingState(
+      {final List<WordWithAnswers> wordsWithAnswers,
+      final int indexCurrentWord,
+      final List<bool> answerTried}) = _$_TestingState;
+
+  @override
+  List<WordWithAnswers> get wordsWithAnswers;
+  @override
+  int get indexCurrentWord;
+  @override
+  List<bool> get answerTried;
+  @override
+  @JsonKey(ignore: true)
+  _$$_TestingStateCopyWith<_$_TestingState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
