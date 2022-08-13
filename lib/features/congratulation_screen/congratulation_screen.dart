a4bb626b08fea6d09447f52ef56edced758516db
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-08-13 14:45:55 +0300

    refactor interface

--- lib/features/congratulation_screen/congratulation_screen.dart
+++ lib/features/congratulation_screen/congratulation_screen.dart
@@ -39,67 +39,54 @@ class _CongratulationScreenState extends State<CongratulationScreen> {
           backgroundColor: Theme.of(context).backgroundColor,
           body: Padding(
             padding: const EdgeInsets.all(15),
-            child: Column(
-              children: [
-                Align(
-                  alignment: Alignment.topRight,
-                  child: IconButton(
-                    onPressed: () {
-                      context.push('/learning');
-                    },
-                    icon: const Icon(Icons.close),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Congratulation! You passed the test!',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline3,
                   ),
-                ),
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Congratulation! You passed the test!',
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.headline3,
-                      ),
-                      const SizedBox(
-                        height: 50,
-                      ),
-                      Text(
-                        '${widget.times} times you answered incorrectly',
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.headline5,
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      if (widget.times > 3)
-                        Column(
-                          children: [
-                            const SizedBox(
-                              height: 30,
-                            ),
-                            Text(
-                              'Maybe you need to go over the words again?',
-                              textAlign: TextAlign.center,
-                              style: Theme.of(context).textTheme.headline5,
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
-                                context.push('/learning');
-                              },
-                              child: const Text(
-                                'back to learning',
-                              ),
-                            ),
-                          ],
-                        )
-                      else
-                        Text(
-                          'Great job!',
-                          style: Theme.of(context).textTheme.headline4,
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Text(
+                    '${widget.times} times you answered incorrectly',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  if (widget.times > 3)
+                    Column(
+                      children: [
+                        const SizedBox(
+                          height: 30,
                         ),
-                    ],
-                  ),
-                ),
-              ],
+                        Text(
+                          'Maybe you need to go over the words again?',
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context).textTheme.headline5,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            context.push('/learning');
+                          },
+                          child: const Text(
+                            'back to learning',
+                          ),
+                        ),
+                      ],
+                    )
+                  else
+                    Text(
+                      'Great job!',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                ],
+              ),
             ),
           ),
         ),
