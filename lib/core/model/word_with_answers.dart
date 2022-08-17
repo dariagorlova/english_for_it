5fe46642dcac54d98d547782e9eaad0b0b8017ab
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-08-17 23:21:43 +0300

    add class for word with state

--- lib/core/model/word_with_answers.dart
+++ lib/core/model/word_with_answers.dart
@@ -1,6 +1,9 @@
+import 'package:english_for_it/core/model/one_word.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'word_with_answers.freezed.dart';
+
+enum WordState { initial, checking, correctly, wrong, tried }
 
 @freezed
 class WordWithAnswers with _$WordWithAnswers {
@@ -9,4 +12,19 @@ class WordWithAnswers with _$WordWithAnswers {
     @Default([]) List<String> answers,
     @Default(0) int indexOfCorrectAnswer,
   }) = _WordWithAnswers;
+}
+
+@freezed
+class WordWithState with _$WordWithState {
+  const factory WordWithState({
+    @Default(
+      OneWord(
+        word: '',
+        translate: '',
+      ),
+    )
+        OneWord word,
+    @Default(WordState.initial)
+        WordState state,
+  }) = _WordWithState;
 }
