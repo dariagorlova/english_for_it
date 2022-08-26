a48015e4e8212b6ddf9c5f54af455f6918f1cc6c
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-08-26 15:38:14 +0300

    refactor test to own db

--- test/utils/mock_repository.dart
+++ test/utils/mock_repository.dart
@@ -1,21 +1,43 @@
-import 'package:english_for_it/core/service/vocabulary.dart';
+//import 'package:english_for_it/core/service/vocabulary.dart';
+import 'package:english_for_it/core/model/one_word.dart';
+import 'package:english_for_it/core/service/daily_words_repository.dart';
 import 'package:mocktail/mocktail.dart';
 
-class MockWordsRepository extends Mock implements Vocabulary {}
+// class MockWordsRepository extends Mock implements Vocabulary {}
 
-Vocabulary getWordsRepository() {
+// Vocabulary getWordsRepository() {
+//   final mock = MockWordsRepository();
+//   when(mock.getAllWords).thenReturn([
+//     'developer',
+//     'computer',
+//     'bug',
+//     'keyboard',
+//     'code',
+//     'task',
+//     'generation',
+//     'repository',
+//     'meeting',
+//     'deadline',
+//   ]);
+//   return mock;
+// }
+class MockWordsRepository extends Mock implements DailyWordsRepository {}
+
+DailyWordsRepository getWordsRepository() {
   final mock = MockWordsRepository();
-  when(mock.getAllWords).thenReturn([
-    'developer',
-    'computer',
-    'bug',
-    'keyboard',
-    'code',
-    'task',
-    'generation',
-    'repository',
-    'meeting',
-    'deadline',
-  ]);
+  when(mock.getDailyWords).thenAnswer((_) async {
+    return const [
+      OneWord(word: 'developer', translate: 'розробник'),
+      OneWord(word: 'computer', translate: 'компʼютер'),
+      OneWord(word: 'bug', translate: 'помилка'),
+      OneWord(word: 'keyboard', translate: 'клавіатура'),
+      OneWord(word: 'code', translate: 'код'),
+      OneWord(word: 'task', translate: 'завдання'),
+      OneWord(word: 'generation', translate: 'генерація'),
+      OneWord(word: 'repository', translate: 'репозиторій'),
+      OneWord(word: 'meeting', translate: 'зустріч'),
+      OneWord(word: 'deadline', translate: 'крайній термін'),
+    ];
+  });
   return mock;
 }
