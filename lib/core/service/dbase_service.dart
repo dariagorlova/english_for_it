fcc559da928ae398dd20b6f9c2e60e9e807de00b
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-08-28 15:52:09 +0300

    remove dbaseService to abstract class

--- lib/core/service/dbase_service.dart
+++ lib/core/service/dbase_service.dart
@@ -1,9 +1,23 @@
+import 'dart:async';
+
 import 'package:english_for_it/core/domain/dbase.dart';
 import 'package:english_for_it/core/model/word_translation_model.dart';
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-class DbaseService {
-  DbaseService({required this.isPermissionsGranted});
+abstract class DbaseService {
+  // Future<bool> checkPermissions();
+  Future<void> connectDb();
+  Future<void> disconnectDb();
+  Future<int> getWordsCount();
+  Future<OneWordPair> getWordByIndex(int index);
+}
+
+@prod
+@Injectable(as: DbaseService)
+class DbaseServiceImpl implements DbaseService, Disposable {
+  DbaseServiceImpl({required this.isPermissionsGranted});
   final _db = DbStorage();
   final bool isPermissionsGranted;
 
@@ -12,33 +26,34 @@ class DbaseService {
     if (status.isDenied) {
       status = await Permission.storage.request();
     }
-    if (!status.isGranted) {
-      return false;
-    }
-    return true;
+    return status.isGranted;
   }
 
+  @override
   Future<void> connectDb() async {
     if (isPermissionsGranted) await _db.openDict();
   }
 
+  @override
   Future<void> disconnectDb() async {
     if (isPermissionsGranted) await _db.closeDict();
   }
 
-  Future<bool> saveWord(OneWordPair line) async {
-    if (isPermissionsGranted && _db.isConnected()) {
-      final res = await _db.saveWord(line);
-      if (line.id == -1) {
-        if (res == 0) return false;
-        line = line.copyWith(id: res);
-        return true;
-      }
-      return res == 1;
-    }
-    return false;
-  }
+  // @override
+  // Future<bool> saveWord(OneWordPair line) async {
+  //   if (isPermissionsGranted && _db.isConnected()) {
+  //     final res = await _db.saveWord(line);
+  //     if (line.id == -1) {
+  //       if (res == 0) return false;
+  //       line = line.copyWith(id: res);
+  //       return true;
+  //     }
+  //     return res == 1;
+  //   }
+  //   return false;
+  // }
 
+  @override
   Future<int> getWordsCount() async {
     if (isPermissionsGranted && _db.isConnected()) {
       final res = await _db.getWordsCount();
@@ -47,14 +62,15 @@ class DbaseService {
     return -1;
   }
 
-  Future<bool> deleteWordById(int id) async {
-    if (isPermissionsGranted && _db.isConnected()) {
-      final res = await _db.deleteWord(id);
-      return res;
-    }
-    return false;
-  }
+  // Future<bool> deleteWordById(int id) async {
+  //   if (isPermissionsGranted && _db.isConnected()) {
+  //     final res = await _db.deleteWord(id);
+  //     return res;
+  //   }
+  //   return false;
+  // }
 
+  @override
   Future<OneWordPair> getWordByIndex(int index) async {
     if (isPermissionsGranted && _db.isConnected()) {
       final res = await _db.getWordByIndex(index);
@@ -67,11 +83,16 @@ class DbaseService {
     );
   }
 
-  Future<List<OneWordPair>> getAllWords() async {
-    if (isPermissionsGranted && _db.isConnected()) {
-      final res = await _db.getAllWords();
-      return res;
-    }
-    return <OneWordPair>[];
+  // Future<List<OneWordPair>> getAllWords() async {
+  //   if (isPermissionsGranted && _db.isConnected()) {
+  //     final res = await _db.getAllWords();
+  //     return res;
+  //   }
+  //   return <OneWordPair>[];
+  // }
+
+  @override
+  FutureOr<void> onDispose() {
+    return _db.closeDict();
   }
 }
