a7fa2c56ec5e4eb2c7aa25953868ad827fe6d7c5
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-09-07 14:55:54 +0300

    delete some old comments

--- lib/core/service/daily_words_repository.dart
+++ lib/core/service/daily_words_repository.dart
@@ -5,44 +5,9 @@ import 'package:injectable/injectable.dart';
 
 @injectable
 class DailyWordsRepository {
-  DailyWordsRepository(
-      //this._dbaseService,
-      //@Named('seed') int seed,
-      //) : _random = Random(seed);
-      );
-
-  //final DbaseService _dbaseService;
-  //final Random _random;
+  DailyWordsRepository();
 
   Future<List<OneWord>> getDailyWords() async {
-    // final dbase = DbaseServiceImpl(
-    //   isPermissionsGranted: await DbaseServiceImpl.checkPermissions(),
-    // );
-    // await dbase.connectDb();
-    // final count = await dbase.getWordsCount();
-    // final listIndex = <int>[];
-    // while (true) {
-    //   final dailyIndex = _random.nextInt(count);
-    //   if (!listIndex.contains(dailyIndex)) {
-    //     listIndex.add(dailyIndex);
-    //   }
-    //   if (listIndex.length == 10) {
-    //     break;
-    //   }
-    // }
-    // final listDailyWords = await Future.wait(
-    //   Iterable.generate(10, (index) {
-    //     return dbase.getWordByIndex(listIndex[index]);
-    //   }),
-    // );
-    // await dbase.disconnectDb();
-    // final list2 = <OneWord>[];
-    // for (final w in listDailyWords) {
-    //   list2.add(
-    //     OneWord(word: w.word, translate: w.translation),
-    //   );
-    // }
-
     final list2 = <OneWord>[];
     final dbase = DbaseServiceImpl(
       isPermissionsGranted: await DbaseServiceImpl.checkPermissions(),
