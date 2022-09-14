6a7924f8f62a2ac4a7fccead618361818aa2dd8b
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-09-14 16:39:37 +0300

    add cubit for phrases sceen

---
+++ lib/features/phrases_screen/cubit/phrases_state.dart
@@ -0,0 +1,30 @@
+import 'package:english_for_it/core/model/phrase.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'phrases_state.freezed.dart';
+
+@freezed
+class PhrasesState with _$PhrasesState {
+  const factory PhrasesState({
+    required List<Phrase> dailyPhrases,
+    required List<String> textForDisplay,
+    @Default(0) int indexCurrenPhrase,
+    @Default(true) bool displayInEnglish,
+    @Default(false) bool sentenceDisplayed,
+  }) = _PhrasesState;
+}
+
+extension XPhrasesState on PhrasesState {
+  int get currentIndex => indexCurrenPhrase;
+
+  Phrase get currentPhrase => dailyPhrases[indexCurrenPhrase];
+
+  //String get textForDisplay => textForScreen;
+
+  bool get isCurrentPhraseFirst => indexCurrenPhrase == 0;
+
+  bool get isCurrentPhraseLast =>
+      indexCurrenPhrase == (dailyPhrases.length - 1);
+
+  bool get isLoading => dailyPhrases.isEmpty;
+}
