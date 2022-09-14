53aaa8b08676151032313051b4e55f0461bdd09b
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-09-14 16:44:13 +0300

    add generated file

--- lib/di/injection.config.dart
+++ lib/di/injection.config.dart
@@ -6,18 +6,17 @@
 
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:translator/translator.dart' as _i6;
+import 'package:translator/translator.dart' as _i5;
 
-import '../core/domain/dbase.dart' as _i5;
-import '../core/model/one_word.dart' as _i9;
-import '../core/service/daily_words_repository.dart' as _i3;
-import '../core/service/dbase_service.dart' as _i4;
-import '../core/service/vocabulary.dart' as _i10;
-import '../features/learning_screen/cubit/learning_cubit.dart' as _i7;
-import '../features/make_pair_screen/cubit/pairs_cubit.dart' as _i8;
-import '../features/testing_screen.dart/cubit/testing_cubit.dart' as _i11;
-import 'google_translator_module.dart' as _i12;
-import 'seed_module.dart' as _i13;
+import '../core/domain/dbase.dart' as _i4;
+import '../core/model/one_word.dart' as _i8;
+import '../core/service/dbase_service.dart' as _i3;
+import '../core/service/vocabulary.dart' as _i9;
+import '../features/learning_screen/cubit/learning_cubit.dart' as _i6;
+import '../features/make_pair_screen/cubit/pairs_cubit.dart' as _i7;
+import '../features/testing_screen.dart/cubit/testing_cubit.dart' as _i10;
+import 'google_translator_module.dart' as _i11;
+import 'seed_module.dart' as _i12;
 
 const String _prod = 'prod';
 // ignore_for_file: unnecessary_lambdas
@@ -28,28 +27,26 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
   final googleTranslatorModule = _$GoogleTranslatorModule();
   final seedModule = _$SeedModule();
-  gh.factory<_i3.DailyWordsRepository>(() => _i3.DailyWordsRepository());
-  gh.factory<_i4.DbaseService>(
-      () => _i4.DbaseServiceImpl(
-          isPermissionsGranted: get<bool>(), db: get<_i5.DbStorage>()),
+  gh.factory<_i3.DbaseService>(
+      () => _i3.DbaseServiceImpl(
+          isPermissionsGranted: get<bool>(), db: get<_i4.DbStorage>()),
       registerFor: {_prod});
-  gh.factory<_i6.GoogleTranslator>(
+  gh.factory<_i5.GoogleTranslator>(
       () => googleTranslatorModule.googleTranslator,
       registerFor: {_prod});
-  gh.factory<_i7.LearningCubit>(
-      () => _i7.LearningCubit(get<_i3.DailyWordsRepository>()));
-  gh.factoryParam<_i8.PairsCubit, List<_i9.OneWord>, dynamic>(
-      (words, _) => _i8.PairsCubit(words));
-  gh.lazySingleton<_i10.Vocabulary>(() => _i10.Vocabulary());
+  gh.factory<_i6.LearningCubit>(() => _i6.LearningCubit(get<dynamic>()));
+  gh.factoryParam<_i7.PairsCubit, List<_i8.OneWord>, dynamic>(
+      (words, _) => _i7.PairsCubit(words));
+  gh.lazySingleton<_i9.Vocabulary>(() => _i9.Vocabulary());
   gh.factory<int>(() => seedModule.seed, instanceName: 'seed');
   gh.factory<int>(() => seedModule.translatesSeed,
       instanceName: 'translates_seed');
-  gh.factoryParam<_i11.TestingCubit, List<_i9.OneWord>, int>(
-      (words, variantENtoUA) => _i11.TestingCubit(
+  gh.factoryParam<_i10.TestingCubit, List<_i8.OneWord>, int>(
+      (words, variantENtoUA) => _i10.TestingCubit(
           get<int>(instanceName: 'translates_seed'), words, variantENtoUA));
   return get;
 }
 
-class _$GoogleTranslatorModule extends _i12.GoogleTranslatorModule {}
+class _$GoogleTranslatorModule extends _i11.GoogleTranslatorModule {}
 
-class _$SeedModule extends _i13.SeedModule {}
+class _$SeedModule extends _i12.SeedModule {}
