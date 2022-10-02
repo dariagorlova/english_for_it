b812d550c27effcfea413d813f80c508eb341381
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-10-02 19:51:17 +0300

    test for search word

--- test/step/i_enter_text_into_text_field.dart
+++ test/step/i_enter_text_into_text_field.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 Future<void> iEnterTextIntoTextField(WidgetTester tester, String text) async {
-  await tester.enterText(find.byType(TextFormField), text);
+  await tester.enterText(find.byType(TextField), text);
   await tester.pumpAndSettle();
 }
