dc1acbdee147307b338c7f3ed70c681a7604f790
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-10-05 09:13:06 +0300

    add some unit tests

---
+++ test/cubit/make_pairs_cubit_test.dart
@@ -0,0 +1,39 @@
+import 'package:english_for_it/core/service/daily_repository.dart';
+import 'package:english_for_it/features/words/make_pair_screen/cubit/pairs_cubit.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockDailyRepository extends Mock implements DailyRepository {}
+
+void main() {
+  final mockWordsRepository = MockDailyRepository();
+  when(mockWordsRepository.getDailyWords).thenAnswer((_) async {
+    return const [
+      // OneWord(word: 'developer', translate: 'розробниця'),
+      // OneWord(word: 'computer', translate: 'компʼютер'),
+      // OneWord(word: 'bug', translate: 'помилка'),
+      // OneWord(word: 'keyboard', translate: 'клавіатура'),
+      // OneWord(word: 'code', translate: 'код'),
+      // OneWord(word: 'task', translate: 'завдання'),
+      // OneWord(word: 'generation', translate: 'генерація'),
+      // OneWord(word: 'repository', translate: 'репозиторій'),
+      // OneWord(word: 'meeting', translate: 'зустріч'),
+      // OneWord(word: 'deadline', translate: 'крайній термін'),
+    ];
+  });
+
+  late final PairsCubit sut;
+  setUp(() async {
+    final words = await mockWordsRepository.getDailyWords();
+    sut = PairsCubit(words);
+  });
+
+  group('Search cubit tests', () {
+    test('Updates state with words list', () async {
+      final words = await mockWordsRepository.getDailyWords();
+      sut.init(words);
+
+      expect(sut.state.wordsOnTheLeft.isEmpty, isTrue);
+    });
+  });
+}
