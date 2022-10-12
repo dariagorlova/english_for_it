84db37a806f928831640a99f4aa411c6a0f83ff0
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-10-12 09:20:28 +0300

    add cubit for start screen

---
+++ lib/features/start_screen/search_word/cubit/start_state.dart
@@ -0,0 +1,8 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'start_state.freezed.dart';
+
+@freezed
+class StartState with _$StartState {
+  const factory StartState() = _StartState;
+}
